308323ff97aee96a9abeec8f57a7a9e1d1fae896
jose7 <grace@GRACEJOSELIN>
2023-04-13 06:30:04 +0530

    Updates

--- src/test/java/testRunner/TestRun.java
+++ src/test/java/testRunner/TestRun.java
@@ -1,17 +1,18 @@
 package testRunner;
 
-//import org.junit.runner.RunWith;
+import org.junit.runner.RunWith;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
 
-
-//@RunWith(Cucumber.class)
-//@CucumberOptions
-//       (
-//		features=".//Features/Login.feature",
-//		glue="stepDefinitions",
-//		dryRun=true,
-//		monochrome=true,
-//		plugin= {"pretty",html:test-output}
-//		)
+@RunWith(Cucumber.class)
+@CucumberOptions
+       (
+		features=".//Features/Login.feature",
+		glue="stepDefinitions",
+		dryRun=true,
+		monochrome=true,
+		plugin= {"pretty","html:test-output"}
+		)
 public class TestRun {		
 	
 }

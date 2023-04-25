0cbe1866de52c3e3a4e69c1050f3bdeb2784135e
jose7 <grace@GRACEJOSELIN>
2023-04-25 16:52:58 +0530

    New Changes

--- src/test/java/apiTesting/HTTPRequests.java
+++ src/test/java/apiTesting/HTTPRequests.java
@@ -1,6 +1,11 @@
 package apiTesting;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
@@ -10,29 +15,31 @@ import java.util.HashMap;
 
 public class HTTPRequests {
 
-	@Test(priority=1)
+	String url = RestAssured.baseURI = "https://reqres.in/api/users/2";
+	RequestSpecification httpRequest = RestAssured.given();	
+	Response response = httpRequest.get("");
+	int statusCode = response.getStatusCode();
+
+	@Test(priority = 2)
 	void getUsers() {
 
 		given()
-
-				.when().get("https://reqres.in/api/users?page=2")
-
-				.then().statusCode(200).body("page", equalTo(2)).log().all();
+		.when().get("https://reqres.in/api/users/2")
+		.then().statusCode(200).log().all();
+	Assert.assertEquals(statusCode /* actual value */, 200 /* expected value */, "InCorrect status code returned");
 	}
 
-	@Test(priority=2)
+	@Test(priority = 1)
 	void createUser() {
-		
-		HashMap data = new HashMap();
-		data.put("name","Issac");
-		data.put("job","Scientist");
-		
-		given()
-		.contentType("application/json")
-		.body(data)
-		.when().post("https://reqres.in/api/users")				
-		.then().statusCode(201).log().all();
 
+		HashMap data = new HashMap();
+		data.put("name", "Issac");
+		data.put("job", "Scientist");
+
+		given().contentType("application/json").body(data).when().post("https://reqres.in/api/users").then()
+				.statusCode(201).log().all();
+		Assert.assertEquals(statusCode /* actual value */, 200 /* expected value */, "InCorrect status code returned");
+ 
 	}
 
 }
